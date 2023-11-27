2ed0572463561bb829bc06c004e0a56b912c37bb
Daniya Yelemessova <daniya@collingsmail.com>
2023-11-27 09:27:49 -0500

    Add first test fot travel constructor

---
+++ README.md
@@ -0,0 +1,2 @@
+1. Add Travel Constructor
+2. 
--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function PlacesYouHaveBeen(location, date, people, notes){
+  this.location = location;
+  this.date = date;
+  this.people = people;
+  this.notes = notes;
+}
 
+const travelOne = new PlacesYouHaveBeen("Las Vegas", "27th of April", ["Lena", "Tanya", "Natalia"], "Lena's Bachelor Party");
+
+console.log(travelOne)
