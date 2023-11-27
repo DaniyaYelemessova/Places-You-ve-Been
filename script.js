2be834cf24a4d13573b67e315b5daf9949ef371d
Daniya Yelemessova <daniya@collingsmail.com>
2023-11-27 09:40:53 -0500

    Create TravelBook to keep all places i have been

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function TravelBook(){
+  this.places = {}
+}
+
+TravelBook.prototype.addPlaces = function(placesYouHaveBeen){
+    this.places[placesYouHaveBeen.location] = placesYouHaveBeen
+}
+
 function PlacesYouHaveBeen(location, date, people, notes){
   this.location = location;
   this.date = date;
@@ -7,8 +15,10 @@ function PlacesYouHaveBeen(location, date, people, notes){
 
 PlacesYouHaveBeen.prototype.locationAndDate = function(){
   return this.location + ": " + this.date;
-}
+};
 
+let travelBook = new TravelBook();
 const travelOne = new PlacesYouHaveBeen("Las Vegas", "27th of April", ["Lena", "Tanya", "Natalia"], "Lena's Bachelor Party");
+travelBook.addPlaces(travelOne);
 
-console.log(travelOne.locationAndDate())
+console.log(travelBook)
