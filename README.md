a26ee178168071d241b01b36dbedc96f0987fd50
Daniya Yelemessova <daniya@collingsmail.com>
2023-11-27 09:34:19 -0500

    Add method to PlacesYouHaveBeen

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 1. Add Travel Constructor
-2. 
+2. Add method to PlacesYouHaveBeen
--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function PlacesYouHaveBeen(location, date, people, notes){
   this.notes = notes;
 }
 
+PlacesYouHaveBeen.prototype.locationAndDate = function(){
+  return this.location + ": " + this.date;
+}
+
 const travelOne = new PlacesYouHaveBeen("Las Vegas", "27th of April", ["Lena", "Tanya", "Natalia"], "Lena's Bachelor Party");
 
-console.log(travelOne)
+console.log(travelOne.locationAndDate())
